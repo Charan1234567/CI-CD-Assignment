aee5084c5907b5312f8bf9a5262f675d64542f54
Saicharan Gurudu <35473753+Charan1234567@users.noreply.github.com>
2019-07-19 00:08:23 -0700

    Update tfbackend.tf

--- terraform/dev/tfbackend.tf
+++ terraform/dev/tfbackend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket  = "terraform-formac1"
+    bucket  = "saicharangurudu"
     key     = "dev.json"
-    region  = "us-west-2"
-    profile = "profile"
+    region  = "us-east-2"
+    profile = "default"
   }
 }
